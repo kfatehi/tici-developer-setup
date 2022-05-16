cc481cea4d11827f85b517bac9d2eaec69447f1b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2022-05-15 22:47:57 -0700

    Update main.js

--- demos/keep_rotation/main.js
+++ demos/keep_rotation/main.js
@@ -3,6 +3,14 @@ const split2 = require('split2');
 const rad2deg = r=>r*(180/Math.PI)
 const proc = spawn('/data/media/developer/src/realsense_t265_stream/build/t265dump');
 proc.on('exit', process.exit);
+
+/*
+this code is pretty terrible, i wonder if ill come back to this idea, if i do, a few things i want to see first:
+1. worth looking this over https://github.com/commaai/openpilot/blob/master/common/transformations/README.md
+2. i had some more thoughts in mind after writing it that i wanted to look at
+ - https://forum.unity.com/threads/quaternion-to-remove-pitch.822768/
+ - Then call Eigen angular distance vs desired orientation (also normalized to single dimension) to get radians, then make this value 0 via controller 
+*/
 proc.stdout.pipe(split2()).on('data', async (data)=>{
   let {roll, pitch} = JSON.parse(data);
   let r = Math.abs(Math.round(rad2deg(roll)))
